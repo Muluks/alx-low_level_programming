f7d8eecc4bf0c4e79837187c03bb80dfbbfcdb5f
Muluks <mdusman206@gmail.com>
2022-10-17 18:23:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
  *
  * Return: 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
