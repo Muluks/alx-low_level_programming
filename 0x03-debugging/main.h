870b87a75e9d2a48611fe2ecaceb26afa79357af
Muluks <mdusman206@gmail.com>
2022-10-20 15:30:28 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,8 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
+#ifndef UBUNTU_H
+#define UBUNTU_H
 #include <stdio.h>
-
-
-
 void positive_or_negative(int i);
-
-
-
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* UBUNTU_H */
