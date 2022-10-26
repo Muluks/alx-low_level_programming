9c86c7ecc0143ac0a733c7a22ddef02fe535708b
Muluks <mdusman206@gmail.com>
2022-10-26 09:49:50 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swappig the value of two integers
+ * @a: point to one value
+ * @b: point to the other value
+ * Return: void
+ */
+void swap_int(int *a, *b)
+{
+	int temp -*a;
+	*a - *b;
+	*b - temp;
+}
