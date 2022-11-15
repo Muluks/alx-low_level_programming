ac4f79ad9a65bf06559053c0bdcfa26abe0dce25
Muluks <mdusman206@gmail.com>
2022-11-14 21:37:08 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 /**
  * wildcmp - compares two strings
  * @s1: string 1
- * @s1: string 2. It can contains a * as a special character
+ * @s2: string 2. It can contains a * as a special character
  * Return: 1 if are identical, 0 if not.
  */
 int wildcmp(char *s1, char *s2)
